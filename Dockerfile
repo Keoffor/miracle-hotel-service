029a4bcd7abdd8daeca3464da02eaac6c3e539ab
keoffor <kenobago@gmail.com>
2025-02-04 09:07:19 -0600

    added dockerfile configuration

---
+++ Dockerfile
@@ -0,0 +1,6 @@
+
+FROM openjdk:17-alpine
+ARG JAR_FILE=target/*.jar
+COPY ${JAR_FILE} app.jar
+EXPOSE 9192
+ENTRYPOINT ["java", "-jar", "/app.jar"]
--- src/main/java/com/miracle/keoffor/config/CorsConfig.java
+++ src/main/java/com/miracle/keoffor/config/CorsConfig.java
@@ -13,6 +13,8 @@ import org.springframework.web.filter.CorsFilter;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 @Configuration
 @EnableWebMvc
@@ -26,6 +28,7 @@ public class CorsConfig {
 
         config.setAllowCredentials(true);
         config.addAllowedOrigin("http://localhost:5173");
+        config.setAllowedOrigins(List.of("http://localhost:8080"));
         config.setAllowedHeaders(Arrays.asList(
                 HttpHeaders.AUTHORIZATION,
                 HttpHeaders.CONTENT_TYPE,
